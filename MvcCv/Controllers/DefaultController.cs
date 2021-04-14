ee07d1c4a22d70b3d5a169a799c5ebc7f9f1f5e3
Mert <mertoktanfb@gmail.com>
2021-04-14 23:52:41 +0300

    update commit

--- MvcCv/Controllers/DefaultController.cs
+++ MvcCv/Controllers/DefaultController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MvcCv.Models.Entity;
 namespace MvcCv.Controllers
 {
     public class DefaultController : Controller
     {
         // GET: Default
+        DbCvEntities db = new DbCvEntities();
         public ActionResult Index()
         {
-            return View();
+            var degerler = db.Tbl_Hakkımda.ToList();
+            return View(degerler);
+        }
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler = db.Tbl_Deneyımlerım.ToList();
+            return PartialView(deneyimler);
+        }
+        public PartialViewResult Eğitimlerim()
+        {
+            var egitimler = db.Tbl_Egitimlerim.ToList();
+            return PartialView(egitimler);
         }
     }
 }
